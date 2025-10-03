7b9003fbbb421e2748f9bf6d739d53ab46ee8d87
Tamannar123 <tamannarana73@gmail.com>
2025-10-03 11:29:58 +0530

    shows movies routes removed.

--- server/routes/showRoutes.js
+++ server/routes/showRoutes.js
@@ -1,7 +1,7 @@
-import express from "express"
-import{getNowPlayingMovies} from "../controllers/showController.js";
-const showRouter= express.Router();
+// import express from "express"
+// import{getNowPlayingMovies} from "../controllers/showController.js";
+// const showRouter= express.Router();
 
-showRouter.get("/now-playing", getNowPlayingMovies)
-
-export default showRouter;
+// showRouter.get("/now-playing", getNowPlayingMovies)
+//
+// export default showRouter;
--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import  connectDB from './configs/db.js'
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
-import showRouter from './routes/showRoutes.js';
+// import showRouter from './routes/showRoutes.js';
 
 
 const app= express();
@@ -18,6 +18,6 @@ app.use(clerkMiddleware())
 app.get ('/',(req,res)=>res.send('server is Live '))
 app.use('/api/inngest',serve({client: inngest, functions}))
 
-app.use('/api/shows',showRouter)
+// app.use('/api/shows',showRouter)
 
 app.listen(port,()=>console.log(`server listening at http://localhost:${port}`));
